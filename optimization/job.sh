5e201f505f5962c149083e1d8bc28d0b2e0f0962
Kai-Wen Cheng <kac003@ucsd.edu>
2022-06-23 13:59:19 -0700

    New Data

--- optimization/job.sh
+++ optimization/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in 15 30 45 60 75 90 105 120 135 150 165 180
+for i in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130 135 140 145 150 155 160 165 170 175 180
 do
   echo "Running P = $i"
-  python EDF_2021.py --P $i
+  python carbon_aware.py --P $i --factor 2
 done
 
